094420380557c1f58e92fecaf4c1aee5fcd05738
kash1897 <kpandya495@gmail.com>
2017-11-21 18:36:20 -0500

    Version 2

--- Login.java
+++ Login.java
@@ -1,122 +1,223 @@
-   /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package coe528.project;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Kobikan2
+ * @author kashyappandya
  */
-import java.io.*;
-import java.util.*;
 public class Login {
-    private String log="login.txt";
-    private String temptxt;
-    private int key;
-    public Login(){
+    public String file = "User_Profiles.txt";
+    
+    public boolean Auth(String x, String select){
+        String line = null;
         
+        try {
+            FileReader Reader = new FileReader(file);
+            BufferedReader writer = new BufferedReader(Reader);
+            
+            while((line = writer.readLine()) != null) {
+                String[] wordLine = line.split(" ");
+                for (String word : wordLine) {
+                    if (x.equals(word)){
+                        return true;
+                    }
+                }
+            }   
+            writer.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("\nUnable to open file.\n\n");                
+        }
+        catch(IOException ex) {
+            System.out.println("\nError reading file.\n\n");
+        }
+        return false;
     }
- public void  Inittxt(File x) throws IOException{
-if(x.exists()==false){
-    x.createNewFile();
-}
- }
- 
- public boolean Authenticate() throws IOException{
-     File login=new File("login.txt");
-             BufferedReader br=null;
-             Scanner scan=new Scanner(System.in);
-             String s,user,pass;
-             String arr[];
-             try{
-                  br=new BufferedReader(new FileReader(log));
-                  BufferedWriter bw=new BufferedWriter(new FileWriter(log,true));
-             }catch(FileNotFoundException e){
-                 System.out.println("not found");
-             }catch(IOException e){
-                 System.out.println("unable to read");
-             }
-      System.out.println("Type your username");
-      user= scan.next();
-      System.out.println("Type your password");
-      pass= scan.next();
-      while((s=br.readLine())!=null){
-          arr=s.split(" ");
-          if(arr[0].equals(user)&&arr[1].equals(pass)){
-              return true;
-          }
-                 }
-      return false;
- }
- public void Create() throws IOException{
-
-             BufferedReader br=null;
-             Scanner scan=new Scanner(System.in);
-             String s,user,pass;
-             KeyGen k=new KeyGen();
-             try{
-                  br=new BufferedReader(new FileReader(log));
-                  BufferedWriter bw=new BufferedWriter(new FileWriter(log,true));
-                     System.out.println("Type your username");
-                     user= scan.next();
-                     System.out.println("Type your password");
-                     pass= scan.next();
-                     if(br.readLine()!=null)
-                     bw.newLine();
-                     bw.write(user+" "+pass);
-                     bw.close();
-                     br.close();
-                     temptxt=user;
-                     key=k.Findp();
-             }catch(FileNotFoundException e){
-                 System.out.println("not found");
-             }catch(IOException e){
-                 System.out.println("unable to read");
-             }
- }
-    public static void main(String[] args) throws IOException {
-             File log=new File("login.txt");
-             Login l =new Login();
-             l.Inittxt(log);
-             BufferedReader br=null;
-             Scanner scan=new Scanner(System.in);
-             String s,userfile;
-             boolean cont=true;
-             EncDec ecr;
-             try{
-                  br=new BufferedReader(new FileReader(log));
-                  BufferedWriter bw=new BufferedWriter(new FileWriter(log,true));
-             }catch(FileNotFoundException e){
-                 System.out.println("not found");
-             }catch(IOException e){
-                 System.out.println("unable to read");
-             }
-             try{
-                 br.close();
-             }catch(IOException e){
-                 System.out.println("");
-             }catch(NullPointerException e){
-                 System.out.println("");
-             }
-             while(cont==true){
-             System.out.println("Do you want to Login or create a new Login(l or c)");
-             s=scan.next();
-             if(s.equals("l")){
-                 l.Authenticate();
-                 ecr=new EncDec();
-                 System.out.println("Enter a string to be encrypted");
-                 String enc=scan.next();
-                 ecr.ModE(enc,l.temptxt,l.key);
-             }
-             else if(s.equals("c")){
-                 l.Create();
-             }
-                 System.out.println("Do you want to Login");
-                 if(scan.next().equals("y"))
-                     cont=true;
-                 else 
-                     cont=false;
-             }
-    }
+    
+    
+    public static void main(String[] args){
+        boolean logout = false; 
+        BufferedWriter writer = null;
+        BufferedReader reader = null;
+        Login main = new Login();
+        
+        try {
+            File file = new File("User_Profiles.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            writer = new BufferedWriter(new FileWriter(file, true));
+            if(!main.Auth("admin", "user")){
+                writer.write("Username: admin Password: pass\n");
+            }
+            writer.close();
+            writer = new BufferedWriter(new FileWriter(file, true));
+        } 
+        catch (IOException ioe) {
+	   ioe.printStackTrace();
+	}
+        
+        do{
+            Scanner sc = new Scanner(System.in);
+            Login login = new Login();
+            
+            System.out.println("Welcome to XYZ Encryption/Decryption and Backup Tool.\n");
+            
+            System.out.println("Enter access level (Administrator or User; Press Enter to exit):");
+            String access = sc.nextLine();
+            if(access.equals("")){
+                break;
+            }
+            while(!(access.equals("Administrator") || access.equals("administrator") || access.equals("User") || access.equals("user"))){
+                System.out.println("\nPlease enter appropriate access level:");
+                access = sc.nextLine();
+            }
+            
+            System.out.println("\nUsername:");
+            String username = sc.nextLine();
+            if(!username.equals("admin")){
+                do{
+                    if(!username.equals("admin")){
+                        if(!login.Auth(username, "user")){
+                            System.out.println("Login denied. Username does not exist.");
+                            System.out.println("\nUsername: ");
+                            username = sc.nextLine();
+                        }
+                    }
+                } 
+                while(!login.Auth(username, "user"));
+            }
+            
+            System.out.println("\nPassword:");
+            String password = sc.nextLine();
+            if(!password.equals("admin") || !password.equals(" ")){
+                do{
+                    if(!login.Auth(password, "pass")){
+                        System.out.println("Login denied. Password does not match.");
+                        System.out.println("\nPassword:");
+                        password = sc.nextLine();
+                    }
+                }
+                while(!login.Auth(password, "pass"));
+            }
+            
+            if(access.equals("Administrator") || access.equals("administrator")){
+                do{
+                    System.out.println("\nSelect:\n1. Add Profile\n2. Remove Profile\n3. Logout");
+                    String ad1 = sc.nextLine();
+                    if(ad1.equals("1")){
+                        System.out.println("\nEnter username: ");
+                        String u = sc.nextLine();
+                        do{
+                            if(!login.Auth(u, "user")){
+                                System.out.println("\nEnter password: ");
+                                String p = sc.nextLine();
+                                try{
+                                    writer.write("Username: " + u + " Password: " + p + " Key: \n");
+                                    writer.close();
+                                    writer = new BufferedWriter(new FileWriter(login.file, true));
+                                }
+                                catch(IOException ioe) {
+                                    ioe.printStackTrace();
+                                }
+                            }
+                            else{
+                                System.out.println("\nUsername already exists.\nPlease try again.\n");
+                            }
+                        }
+                        while(!login.Auth(u, "user"));
+                    }
+                    
+                    if(ad1.equals("2")){
+                        System.out.println("\nEnter username of profile to be removed: ");
+                        String remove = sc.nextLine();
+                        String line;
+                        if(login.Auth(remove, "user")){
+                            try {
+                                File in = new File(login.file);
+                                if (!in.isFile()) {
+                                    System.out.println("Parameter is not an existing file");
+                                    break;
+                                }
+                                File tFile = new File(in.getAbsolutePath() + ".tmp");
+                                BufferedReader buffreader = new BufferedReader(new FileReader(login.file));
+                                PrintWriter pwriter = new PrintWriter(new FileWriter(tFile));
+          
+                                while ((line = buffreader.readLine()) != null) {
+                                    if (!line.trim().contains("Username: " + remove)) {
+                                        pwriter.println(line);
+                                        pwriter.flush();
+                                    }
+                                }
+                                pwriter.close();
+                                buffreader.close();
+                                if (!in.delete()) {
+                                    System.out.println("Could not delete file");
+                                    return;
+                                }
+                                if (!tFile.renameTo(in))
+                                    System.out.println("Could not rename file");
+                            } catch (FileNotFoundException ex) {
+                                ex.printStackTrace();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                            System.out.println("\nUser has been removed.");
+                        }  
+                        else{
+                            System.out.println("\nUser does not exist.");
+                        }
+                    }
+                    
+                    if(ad1.equals("3")){
+                        logout = true;
+                        System.out.println("\nLogout Successful.");
+                    }
+                }
+                while(logout != true);
+            }
+            
+            if(access.equals("User") || access.equals("user")){
+                System.out.println("Select:\n1. Encryption\n2. Decryption\n3. Backup");
+                String uc = sc.nextLine();
+                
+                if(uc.equals("1")){
+                    
+                }
+                
+                if(uc.equals("2")){
+                    
+                }
+                
+                if(uc.equals("3")){
+                    
+                }
+            }
+            
+            System.out.println("\n");
+        }
+        while(logout == true);
+        
+        try{
+            writer.close();
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        
+        System.out.println("\nThank you for using the tool.");   
+    }  
 }
